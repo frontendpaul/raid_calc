7314929f31a227aeb0c7e312b71a3a579b6f9726
506177656c <matizbrave@gmail.com>
2019-06-30 00:00:51 +0200

    Rewrited calc()

--- app.js
+++ app.js
@@ -11,55 +11,32 @@ document.getElementById("users4star").addEventListener('blur', calc);
 document.getElementById("users5star").addEventListener('blur', calc);
 
 function calc() {
-  users2star = document.getElementById("users2star");
-  users3star = document.getElementById("users3star");
-  users4star = document.getElementById("users4star");
-  users5star = document.getElementById("users5star");
+  let usersStars = [];
+  let neededStars = [];
 
-  if (!users2star.value) {
-    users2star = 0;
-  }
-  else {
-    users2star = parseInt(users2star.value);
-  }
-  if (!users3star.value) {
-    users3star = 0;
-  }
-  else {
-    users3star = parseInt(users3star.value);
-  }
-  if (!users4star.value) {
-    users4star = 0;
-  }
-  else {
-    users4star = parseInt(users4star.value);
-  }
-  if (!users5star.value) {
-    users5star = 0;
-  }
-  else {
-    users5star = parseInt(users5star.value);
+  for (let i = 2; i <= 5; i++) {
+    usersStars[i] = document.getElementById(`users${i}star`);
+
+    if (!usersStars[i].value) {
+      usersStars[i] = 0;
+    }
+    else {
+      usersStars[i] = parseInt(usersStars[i].value);
+    }
   }
 
-  let needed5star = 5 - users5star;
-  if (needed5star < 0) needed5star = 0;
-  document.getElementById("max4stars").innerHTML = needed5star;
-  document.getElementById("output5star").innerHTML = needed5star;
-
-  let needed4star = needed5star * 5 - users4star;
-  if (needed4star < 0) needed4star = 0;
-  document.getElementById("max3stars").innerHTML = needed4star;
-  document.getElementById("output4star").innerHTML = needed4star;
-
-  let needed3star = needed4star * 4 - users3star;
-  if (needed3star < 0) needed3star = 0;
-  document.getElementById("max2stars").innerHTML = needed3star;
-  document.getElementById("output3star").innerHTML = needed3star;
-
-  let needed2star = needed3star * 3 - users2star;
-  if (needed2star < 0) needed2star = 0;
-  document.getElementById("output2star").innerHTML = needed2star;
-
-  // console.log(document.getElementById("champion-select").value);
+  for (let i = 5; i >= 2; i--) {
+    if (i === 5) neededStars[i] = i - usersStars[i];
+    else { neededStars[i] = neededStars[i + 1] * (i + 1) - usersStars[i]; }
+    if (neededStars[i] < 0) neededStars[i] = 0;
+    console.log(neededStars);
+    if (i !== 2) {
+      document.getElementById(`output${i}star`).innerHTML = neededStars[i];
+      document.getElementById(`max${i - 1}stars`).innerHTML = neededStars[i];
+    }
+    else {
+      document.getElementById(`output${i}star`).innerHTML = neededStars[i];
+    }
+  }
 }
 
